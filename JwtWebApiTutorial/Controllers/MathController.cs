e2b91e2f46489c2a2b817716a9c69e8e405fd104
Andrei Xavier <andrei_xavier@outlook.com>
2023-11-22 16:18:53 -0300

    first commit

---
+++ JwtWebApiTutorial/Controllers/MathController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtWebApiTutorial.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class MathController : Controller
+    {
+        [HttpGet("sum")]
+        public ActionResult<int> Sum(int a, int b)
+        {
+            // Se o código chegar aqui, o usuário está autenticado
+            return Ok(a + b);
+        }
+    }
+}
